2591b243ac45fd9f435471c92c588b6b2929aec9
Luiz <lui-otavio@hotmail.com>
2025-02-25 11:37:10 -0300

    \z

--- Ruiz_Store/Data/AppDbContext.cs
+++ Ruiz_Store/Data/AppDbContext.cs
@@ -12,4 +12,19 @@ public class AppDbContext : IdentityDbContext<Usuario>
         public DbSet<Produtos> Produtos  { get; set; }
         public DbSet<ProdutoFotos> ProdutoFotos { get; set; }
         public DbSet<Usuarios> Usuarios {get; set;}
+
+        protected override void OnModelCreating(ModelBuild builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region Definindo nomes de Identity
+            builder.Entity<IdentityUser>().ToTable("Usuario");
+            builder.Entity<IdentityRole>().ToTable("Perfil");
+            builder.Entity<IdentityUserRole<string>>().ToTable("Usuario_perfil");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("Usuario_regra");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("Usuario_login");
+            builder.Entity<IdentityUserToken<string>>().ToTable("Usuario_token");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("perfil_regra");
+            #endregion
+        } 
 }
--- Ruiz_Store/Program.cs
+++ Ruiz_Store/Program.cs
@@ -3,7 +3,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Serviço de conexão
+string conexao = builder.Configuration.GetConnectionString("Hiena");
+builder.Services.AddDbContext<AppDbCotext>()(
+    options => options.UseMySQL(conexao)
+);
+
+// Serviço de identidade do usuário
+builder.Services.AddIdentity<Usuario, IdentityRole>(
+    options => options.SignIn.RequireConfirmedEmail = false
+).
+.AddIdentityFrameworkStore<AppDbContext>()
+.AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
